9c5e02b8ebe6a9b00b2461e645dfceda02cd1f61
OnyanPokon <mohamadrafiqd@gmail.com>
2025-06-08 12:41:05 +0800

    batman

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div className="grid w-full grid-cols-12 gap-4">wellcome</div>;
+};
+
+export default Dashboard;
---
+++ src/pages/landing/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <section className="mx-auto flex w-full max-w-screen-xl items-center justify-center gap-x-10 px-6 py-28 text-3xl">Wellcome</section>
+    </>
+  );
+};
+
+export default Home;
